2b7e472ff2e3d222a9841c008b7495a1578565bf
Muhammad Atiq <64276263+maybeAtiq@users.noreply.github.com>
2025-05-13 08:55:53 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 const Footer = () => {
 
   return (
-      <div id="footer" className="bg-gray-900 mx-auto w-full py-10 px-4 md:px-20 flex text-center justify-between text-white">
+      <div id="footer" className="bg-gray-900 mx-auto w-full py-10 px-4 md:px-20 flex text-center justify-between text-gray-400">
         <span >© Muhammad Atiq</span>
         <a href="#" target="_blank" rel="noopener noreferrer" className="flex gap-2 items-center">
             Source code
